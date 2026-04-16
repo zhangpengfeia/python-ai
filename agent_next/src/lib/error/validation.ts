eed17f5779fa52fed8ff7de0f8a77d0ec4fc2b29
zhangpf1 <zhangpengfei@csc.com.cn>
2026-04-16 17:38:02 +0800

    fix

--- agent_next/src/lib/error/validation.ts
+++
@@ -1,9 +0,0 @@
-export class ValidationError extends Error {
-    status = 400;
-    constructor(message?: string) {
-      super(message);
-    }
-    serialize() {
-      return { message: this.message, status: this.status };
-    }
-  }
