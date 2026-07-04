9b184085ca9208d141cadcde75557baee6a88eb5
zhangpf1 <zhangpengfei@csc.com.cn>
2026-07-04 22:17:09 +0800

    java: 异常处理，反射，泛型

--- 4.java基础/demo.java
+++ 4.java基础/demo.java
@@ -1,9 +1,3 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 public class demo {
     /**
     数据类型：
@@ -119,9 +113,201 @@ public class demo {
                 使用父类引用调用方法时，只能调用父类中声明的方法，不能调用子类独有方法
                 向下转型应使用instanceof检查，否则可能会报错
                 多态只针对方法，不针对属性
+    */
+    /*
+    抽象类和接口：
+        抽象类：不能实例化，用于定义公共方法，由子类实现，单继承
+        接口：不能实例化，只能被实现，多实现
+     + 抽象类：当多个类有共同代码时，使用抽象类。
+     + 接口：当需要定义一种行为契约，而且不同类型的类都需要实现时，使用接口。
+     + 通常优先接口，以支持更多灵活设计
+    */
+    /* 抽象类
+        子类必须实现所有抽象方法？（问题）
+        不能实例化，只能被继承
+        可以包含具体方法，供子类直接使用或重写
+        可以有构造方法
+        可以有任意类型的成员变量
+    */ 
+    abstract class Animal {
+        // 抽象方法
+        abstract void eat();
+        // 非抽象方法
+        public void sleep() {
+            System.out.println("sleep");
+        }
+    }
+    /*
+    接口
+      完全抽象的类型，只包含常量和抽象方法
+      类继承只能有一个，接口可以多个
+    */
+    interface Drawable {
+        String TYPE = "drawable";
+        //抽象方法： public abstract 可以省略
+        void draw();
+        // 定义默认方法，可以选择是否重写
+        default void draw2() {
+            System.out.println("draw2");
+        }
+    }
+    // 实现接口 implements
+    class Circle implements Drawable {
+        @Override
+        public void draw() {
+            System.out.println("draw circle");
+        }
+    }
 
+    /*
+    包package:
+        用于组织类和接口的机制，避免命名冲突，将功能相关的类分组在一起，形成一个命名空间
+        命名规范：全部小写，域名倒序，有意义的名字，不下划线开头
+        例如：com.company.project
+        
+        包描述文件：package-info.java，用于描述包的元数据，例如包的作者、版本、描述等
+        包注解：@Deprecated，用于标记包为过时，建议使用其他包代替
+        导入：
+            import java.io.* 用于导入java.io包下的所有类,不建议
+            import java.util.List 精确导入，推荐
+    
+    额外了解：模块描述文件module-info.java，只能有一个
     */
 
-          
+    /*
+    异常：java中的异常
+        运行时异常，编译时异常
+    注意：
+        不要再finally中使用return，否则会覆盖try中的return语句
+        finally中抛出的异常会覆盖try中的异常
+    
+    抛出异常语法：throw new IllegalArgumentException("参数错误");
+    throws可以声明多个异常类型，用逗号分隔。
+    子类重写父类方法时，抛出的异常不能比父类宽泛。
+    自定义异常类：继承Exception检查类异常，RuntimeException非检查类异常
+    // try-with-resources方式，代码执行完后自动关闭资源，自动调用close()
+      try (InputStream is = new FileInputStream("test.txt")) {
+        byte[] bytes = new byte[1024];
+        int len = is.read(bytes);
+        System.out.println(new String(bytes, 0, len));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    */
+
+
+    
+    /*
+    反射：
+        1.是java的一种运行机制，允许程程序运行时获取任何内部信息（如构造方法，成员方法，字段，注解等）
+        2.可以动态创建调用对象的方法，访问对象的属性。
+        3.反射使java有了动态性
+    为什么需要：
+        1.允许在运行时才决定操作哪个类，哪个方法，实现更加灵活的程序设计。
+    优点：
+        1.动态性
+        2.框架基础底层
+        3.通用功能实现
+    缺点：
+        1.性能开销，比常规调用多一步。
+        2.破坏封装，可以访问私有成员
+        3.安全隐患，可以绕过访问控制，可能引发安全问题
+        4.代码可读性差
+        5.编译时检查缺失。
+    典型场景：
+        1.开发框架，spring依赖注入，aop
+        2.orm框架，数据库映射到java类
+        3.单元测试
+        4.动态代理
+        5.配置文件解析
+        6.开发工具
+    */
+    // 获取Class对象
+    // 1.通过类名获取
+    Class<Integer> clazz = int.class; // 基本数据类型也可以
+    // 2.通过对象获取
+    Student s = new Student();
+    Class<Student> clazz2 = s.getClass(); // 运行时类型
+    // 3.最主流，通过 Class.forName() 方法获取
+    try {
+        Class<?> clazz3 = Class.forName("com.company.project.Student");
+    } catch (ClassNotFoundException e) {
+        e.printStackTrace();
+    }
+    /*
+    获取构造方法：
+    getConstructors()
+    getDeclaredConstructors()
+    获取成员方法：
+    getMethods()
+    getDeclaredMethods()
+    获取字段：
+    getFields()
+    getDeclaredFields()
+    */
+
+    //==================================================
+
+    /*
+    泛型Generics：
+    1.允许在定义类，接口和方法使用类型参数
+    2.本质是参数化类型，即类型本事可以作为参数传递
+    为什么需要：
+    1.类型不安全
+    2.需要强制类型转换
+    泛型名词约定：
+    T:类型参数，用于表示任意类型
+    E:元素类型，用于表示集合中的元素类型
+    K:键类型，用于表示键值对中的键类型
+    V:值类型，用于表示键值对中的值类型
+
+    注意：
+    不允许泛型实例化
+    不允许创建数组 T[] arr = new T[10]; ❌
+    
+    */
+    // list
+    List<Integer> list = new ArrayList<>();
+
+    //自定义泛型类
+    class MyList<T> {
+        private T content;
+        // 构造方法
+        public MyList(T content) {
+            this.content = content;
+        }
+        // getter方法
+        public T getContent() {
+            return content;
+        }
+        public static void main(String[] args) {
+            MyList<Integer> list = new MyList<>(10);
+            System.out.println(list.getContent());
+            // 10
+            MyList<String> list2 = new MyList<>("hello");
+            System.out.println(list2.getContent());
+            // hello
+        }
+    }
+    // 自定义交互方法的泛型
+    public static <T> void swap(T a, T b) {
+        T temp = a;
+        a = b;
+        b = temp;
+    }
+    // 自定义泛型接口
+    interface MyComparator<k,v> {
+        k getKey();
+        v getValue();
+    }
+    //类型通配符，使用?表示未知类型，不能添加元素（除了null)，类型未知无法保证安全
+    public static void print(List<?> list) {
+        for (Object obj : list) {
+            System.out.println(obj);
+        }
+    }
+    //上界通配符:<? extends T> 表示类型必须是T的子类
+    //下界通配符:<? super T> 表示类型必须是T或者T的父类
+
 
 }
