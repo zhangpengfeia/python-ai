4669fb712b0d1a707b588232188b572f9887b74e
zhangpf1 <zhangpengfei@csc.com.cn>
2026-07-19 17:00:06 +0800

    java

---
+++ 4.java基础/demo.java
@@ -0,0 +1,13 @@
+public class demo {
+    public static void main(String[] args) {
+        
+        // 枚举类型
+        enum Color {
+            RED, GREEN, BLUE
+        }
+        
+        // 枚举类型的使用
+        Color color = Color.RED;
+        System.out.println(color);
+    }
+}
